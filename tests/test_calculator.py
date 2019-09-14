d7223e173ce150c03063c051a58153ab03b57292
abenben <abenbenben@gmail.com>
2019-09-15 02:33:24 +0900

    修正

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -22,7 +22,7 @@ multiplication_lists = (
 
 division_lists = (
     [0, 1, 0],
-    [1, 1, 0],
+    [1, 1, 1],
     [10, 5, 2])
 
 
@@ -67,7 +67,7 @@ def test_multiplication_normal(multiplication_task):
 def division_task(request):
     return request.param
 
-# def test_division_normal(division_task):
-#    cal = calculator.Calculator()
-#    result = cal.division(division_task[0],division_task[1])
-#    assert result == division_task[2]
+def test_division_normal(division_task):
+    cal = calculator.Calculator()
+    result = cal.division(division_task[0],division_task[1])
+    assert result == division_task[2]
